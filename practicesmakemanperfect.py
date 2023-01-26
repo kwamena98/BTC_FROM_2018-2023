dcbfc343c86a2d955c16d46fc9d8c374226e89ca
kwamena98 <derrickdadson14@gmail.com>
2023-01-26 08:44:39 +0000

    first

---
+++ practicesmakemanperfect.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+
+df=pd.read_csv("12 Well-known Crypto(currency)  2018-2023.csv",index_col=False)
+
+BTC=df[df.Type=="Bitcoin USD"]
+ETH=df[df.Type=="Ethereum USD"]
+
+
+btc_highs=BTC.High.values
+eth_highs=ETH.High.values
+
+BTC.loc[:, ('Date')] = pd.to_datetime(BTC['Date'])
+years=BTC.Date.dt.year.values
+
+
+btc_dates=BTC.Date
+
+
+plt.plot(btc_dates,btc_highs,color='orange',label="BTC")
+plt.plot(btc_dates,eth_highs,color='purple',label="ETH")
+
+plt.ylabel("BTC and ETH")
+plt.xlabel("DATE")
+
+plt.legend()
+plt.show()
+
